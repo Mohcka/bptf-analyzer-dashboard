1c08d3352e0c8f071eff338df58839a0621f1692
Mohcka <MohckaMas@proton.me>
2025-03-04 14:48:01 -0500

    Add conditional logging for DATABASE_URL in development environment

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+// show if dev env
+if (process.env.NODE_ENV === 'development') {
+  console.log(process.env.DATABASE_URL);
+}
 
 
 export const db = drizzle(process.env.DATABASE_URL!);
